ee50fc54636fcd5be01e01bea534dcd3a33932bc
Vishal Dhakal <vishaldhakal@Vishals-Air.domain.name>
2020-05-15 10:08:35 +0545

    plot added

--- tutorial/quickstart/urls.py
+++ tutorial/quickstart/urls.py
@@ -4,5 +4,6 @@ from  .import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('plot/', views.plot,name='plot'),
     path('crud_ajax/', views.crud_ajax_create,name='crud_ajax_create'),
 ]
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, HttpResponse
 from .models import userModel
 from django.http import JsonResponse
 import json
+import matplotlib.pyplot as plt
+import io
+import urllib, base64
+import csv
 
 
 def index(request):
@@ -23,3 +27,21 @@ def crud_ajax_create(request):
     }
     l.append(data)
     return HttpResponse(json.dumps(l))
+
+def plot(request):
+    ticks = [0,1,2,3,4,5,6,7,8,9]
+    labels = ['0B','1B','2B','3B','4B','5B','6B','7B','8B','9B']
+    list1 = [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]
+    list2 = [2.3,3.5,4.7,3.5,3.8,3.0,5.0,5.1,6.7,7.1]
+    plt.bar(list1,list2)
+    plt.xlabel('Year in AD')
+    plt.ylabel('Population')
+    plt.title('Population growth as per year')
+    plt.yticks(ticks, labels)
+    fig = plt.gcf()
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    base = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(base)
+    return render(request,'home.html',{'data':uri})
